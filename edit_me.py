e02da0362ceb9225903124ab55bfedcc8767b63f
zmckinnis <zmckinnis@drury.edu>
2025-01-22 11:57:57 -0600

    Current max program

--- edit_me.py
+++ edit_me.py
@@ -6,8 +6,13 @@ def find_max(lst):
 
     #
     #  YOUR CODE GOES HERE
-    #
+    # return max element py Zach McKinnis
 
+    for i in range(1, len(lst)):
+        if lst[i] > current_max:
+            current_max = lst[i]
+
+    
     return current_max
 
 test_list = [2112*i % 2024 for i in range(10000)]
